2b4fad226e04c5e1fa3e0fa321e0dcc9964b66e0
Ali <a.a.ghanati@gmail.com>
2021-11-20 12:57:35 +0330

    extracted navbar and footer from default layout into components

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,45 +1,11 @@
 <template>
-  <v-app dark>
-    <v-navigation-drawer v-model="drawer" right fixed app>
-      <div class="theme-chooser">
-        <v-icon> mdi-weather-{{ dark ? 'sunny' : 'night' }} </v-icon>
-        <v-switch v-model="dark" @click="toggleTheme"></v-switch>
-      </div>
-    </v-navigation-drawer>
-    <v-app-bar fixed app>
-      <v-spacer></v-spacer>
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-    </v-app-bar>
+  <v-app>
+    <TheNavbar />
     <v-main>
       <v-container>
         <Nuxt />
       </v-container>
     </v-main>
-    <v-footer absolute app>
-      <span>&copy; {{ new Date().getFullYear() }}</span>
-    </v-footer>
+    <TheFooter />
   </v-app>
 </template>
-
-<script>
-export default {
-  data() {
-    return {
-      drawer: false,
-      dark: true,
-    }
-  },
-  methods: {
-    toggleTheme() {
-      this.$vuetify.theme.dark = this.dark
-    },
-  },
-}
-</script>
-
-<style scoped>
-.theme-chooser {
-  display: flex;
-  justify-content: center;
-}
-</style>
