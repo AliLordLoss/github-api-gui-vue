0ead0793bf346179d12a2e2c7eb248dd2a4e1884
Ali <a.a.ghanati@gmail.com>
2021-11-20 12:51:14 +0330

    refactored various components and extracted common parts to make them reusable, completed fetch methods in store that fetch and store user data, added pages for followers and following and displayed user data in the home page if there is a user, with links to following and followers page and a link to github official repository page for user repos - getting data with access token is not implemented yet

--- pages/index.vue
+++ pages/index.vue
@@ -2,23 +2,7 @@
   <div>
     <v-container>
       <UserForm v-if="!$store.getters.loggedIn" />
-      <div v-else>
-        username: {{ this.$store.state.username }}
-        <hr />
-        token: {{ this.$store.state.accessToken }}
-        <hr />
-        <v-btn text @click="clearData">Clear My Data</v-btn>
-      </div>
+      <UserCard v-else />
     </v-container>
   </div>
 </template>
-
-<script>
-export default {
-  methods: {
-    clearData() {
-      this.$store.dispatch('clearData')
-    },
-  },
-}
-</script>
--- store/index.js
+++ store/index.js
@@ -1,25 +1,48 @@
 export const state = () => ({
   username: '',
-  accessToken: ''
+  accessToken: '',
+  user: {},
+  following: [],
+  followers: []
 })
 
 export const getters = {
   loggedIn(state) {
     return !!(state.username || state.accessToken)
   },
-
-
 }
 
 export const actions = {
   fetchUserData({ dispatch }, { username, accessToken }) {
-    if (username) dispatch('fetchUserDataWithUsername', { username })
-    else if (accessToken) dispatch('fetchUserDataWithAccessToken', { accessToken })
+    if (username) return dispatch('fetchUserDataWithUsername', { username })
+    else if (accessToken) return dispatch('fetchUserDataWithAccessToken', { accessToken })
   },
 
-  fetchUserDataWithUsername({ commit }, { username }) {
+  async fetchUserDataWithUsername({ commit }, { username }) {
+    const data = await this.$axios.$get(`https://api.github.com/users/${username}`)
+    const repos = await this.$axios.$get(`https://api.github.com/users/${username}/repos`)
+    const user = {
+      avatarUrl: data.avatar_url,
+      following: data.following,
+      followers: data.followers,
+      repos,
+      username,
+    }
     localStorage.setItem('username', username)
     commit('SET_USERNAME', { username })
+    commit('SET_USER_DATA', { user })
+  },
+
+  async fetchUserFollowing({ state, commit }) {
+    let following = await this.$axios.$get(`https://api.github.com/users/${state.user.username}/following`)
+    following = following.map((person) => ({ login: person.login, avatarUrl: person.avatar_url }))
+    commit('SET_FOLLOWING', { following })
+  },
+
+  async fetchUserFollowers({ state, commit }) {
+    let followers = await this.$axios.$get(`https://api.github.com/users/${state.user.username}/followers`)
+    followers = followers.map((person) => ({ login: person.login, avatarUrl: person.avatar_url }))
+    commit('SET_FOLLOWERS', { followers })
   },
 
   fetchUserDataWithAccessToken({ commit }, { accessToken }) {
@@ -30,7 +53,7 @@ export const actions = {
   clearData({ commit }) {
     localStorage.removeItem('accessToken')
     localStorage.removeItem('username')
-    commit('CLEAR_CREDENTIALS')
+    commit('CLEAR_STATE')
   }
 }
 
@@ -43,8 +66,23 @@ export const mutations = {
     state.accessToken = accessToken
   },
 
-  CLEAR_CREDENTIALS(state) {
+  SET_USER_DATA(state, { user }) {
+    state.user = user
+  },
+
+  SET_FOLLOWING(state, { following }) {
+    state.following = following
+  },
+
+  SET_FOLLOWERS(state, { followers }) {
+    state.followers = followers
+  },
+
+  CLEAR_STATE(state) {
     state.username = ''
     state.accessToken = ''
-  }
+    state.user = {}
+    state.followers = []
+    state.following = []
+  },
 }
